3db3ca0747480238b907254ce1525c4525fc3c99
EmreKocak93 <160043074+EmreKocak93@users.noreply.github.com>
2024-09-06 12:06:47 +0300

    Initial commit

---
+++ MvcProjeKampi/DataAccessLayer/Abstract/ICategoryDal.cs
@@ -0,0 +1,22 @@
+﻿using EntitiyLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface ICategoryDal:IRepository<Category>
+    {
+        //CRUD İŞLEMLERİ BURADA YAPILACAK
+        //Type Name();
+        //List yerine Listelemede veya başka birşeyde yazılabilir ingilizce bir ifade olsun diye böyle yaptık.Kafan Karışmasın
+        //List<Category> List();
+        //void Insert(Category p);
+        //void Update(Category p);
+        //void Delete(Category p); 
+
+        //IRepositoryden kalıtım alarak yukarıdaki hiç bir şeye gerek kalmadı .
+    }
+}
